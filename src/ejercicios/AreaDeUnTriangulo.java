f795ed195afa59ef02e78c99df0a5f8d735b8944
Akmeteam <Francisco.Contreras@tsoftglobal.com>
2024-09-05 14:49:51 -0400

    Area de un triangulo.

---
+++ src/ejercicios/AreaDeUnTriangulo.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class AreaDeUnTriangulo {
+    public static void main(String[] args) {
+        double base = 5;
+        double altura = 8;
+        // Aqui se calcula el área usando la formula: (base * altura) / 2
+        double area = (base * altura) / 2;
+        // Aqui se muestra el resultado.
+        System.out.println("El área del triángulo es: " + area);
+    }
+}
