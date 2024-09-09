81443f64f6d43e7ab523d302793178392dbd3429
Akmeteam <akmeteam@gmail.com.com>
2024-09-09 16:55:01 -0300

    Se actualiza codigo de conteo de digitos.

--- src/ejercicios/ConteoDeDigitos.java
+++ src/ejercicios/ConteoDeDigitos.java
@@ -3,7 +3,9 @@ package ejercicios;
 public class ConteoDeDigitos {
     public static void main(String[] args) {
         int numero = 12345;
+        int numeroOriginal = numero; // Guardamos el valor original del número
         int contador = 0;
+
         // Caso especial para el número 0
         if (numero == 0) {
             contador = 1;
@@ -14,6 +16,7 @@ public class ConteoDeDigitos {
                 contador++;
             }
         }
-        System.out.println("El número " + numero + " tiene " + contador + " dígitos.");
+
+        System.out.println("El número " + numeroOriginal + " tiene " + contador + " dígitos.");
     }
 }
